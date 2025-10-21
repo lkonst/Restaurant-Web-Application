e6e0f7270ae7130b7cd30c83a6e4f57f5eaff150
lkonst <fisikosnomos@gmail.com>
2025-10-21 09:19:39 +0300

    Refactor views and update URL routing to use SingleMenuItemView and add protected message endpoint

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MenuItemView, MenuItemDetailView
+from .views import MenuItemView, SingleMenuItemView, msg
 
 urlpatterns = [
     path("menu-items/", MenuItemView.as_view(), name="menu-item-list"),
-    path("menu-items/<int:pk>/", MenuItemDetailView.as_view(), name="menu-item-detail"),
+    path("menu-items/<int:pk>/", SingleMenuItemView.as_view(), name="menu-item-detail"),
+    path('message/', msg),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -13,8 +16,15 @@ class MenuItemView(ListCreateAPIView):
     serializer_class = MenuItemSerializer
 
 
-class MenuItemDetailView(RetrieveUpdateDestroyAPIView):
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     """GET one, PUT/PATCH update, DELETE"""
 
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message": "This view is protected"})
