807a46de9449e7a2f642a2128867dd2cc1895fa2
lkonst <fisikosnomos@gmail.com>
2025-10-21 09:46:31 +0300

    Add UserSerializer to handle user data serialization

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import MenuItem
+from django.contrib.auth.models import User
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ["id", "title", "price", "inventory"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "groups"]
