f6e0ed64b8ae2584bac26256453424c728850340
lkonst <fisikosnomos@gmail.com>
2025-10-24 23:37:12 +0300

    Fix URL pattern for SingleMenuItemView by removing trailing slash

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -3,6 +3,6 @@ from .views import MenuItemsView, SingleMenuItemView, msg
 
 urlpatterns = [
     path("menu-items/", MenuItemsView.as_view(), name="menu-item-list"),
-    path("menu-items/<int:pk>/", SingleMenuItemView.as_view(), name="menu-item-detail"),
+    path("menu-items/<int:pk>", SingleMenuItemView.as_view(), name="menu-item-detail"),
     path("message/", msg),
 ]
